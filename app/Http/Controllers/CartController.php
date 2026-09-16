9d99a9ea7527b78a5209a2dadcf98e03ae26e42a
INDRATHEDESTROYER <D308873@edu.curio.nl>
2026-09-16 10:32:23 +0200

    Inloggen systeem en winkelmandje toegevoegd

---
+++ app/Http/Controllers/CartController.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Product;
+
+class CartController extends Controller
+{
+    public function index()
+    {
+        $cart = session()->get('cart', []);
+
+        return view('cart.index', compact('cart'));
+    }
+
+    public function add(Product $product)
+    {
+        $cart = session()->get('cart', []);
+
+        if (isset($cart[$product->id])) {
+            $cart[$product->id]['quantity']++;
+        } else {
+            $cart[$product->id] = [
+                'name' => $product->name,
+                'price' => $product->price,
+                'quantity' => 1
+            ];
+        }
+
+        session()->put('cart', $cart);
+
+        return redirect()->route('cart.index');
+    }
+
+    public function remove(Product $product)
+    {
+        $cart = session()->get('cart', []);
+
+        unset($cart[$product->id]);
+
+        session()->put('cart', $cart);
+
+        return redirect()->route('cart.index');
+    }
+}
