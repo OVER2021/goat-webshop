45807d24ea3fd574f47f303ad356956d5b8ec1e2
INDRATHEDESTROYER <D308873@edu.curio.nl>
2026-09-17 11:38:51 +0200

    Revert "Inloggen systeem en winkelmandje toegevoegd"
    This reverts commit 9d99a9ea7527b78a5209a2dadcf98e03ae26e42a.

--- resources/views/products/index.blade.php
+++ resources/views/products/index.blade.php
@@ -1,22 +1,28 @@
 <!DOCTYPE html>
 <html lang="nl">
-
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
     <title>Producten</title>
+
     @vite(['resources/css/app.css'])
 </head>
 
 <body>
 
     <header class="hero">
-
         <img
             src="{{ asset('img/test/logowebshop.png') }}"
             alt="Logo"
             class="logo"
         >
+
+        <!-- <img
+            src="{{ asset('img/poot2.png') }}"
+            alt="Geit"
+            class="goat goat-right"
+        > -->
 
         <img
             src="{{ asset('img/test/test4.png') }}"
@@ -47,62 +53,20 @@
         <img src="{{ asset('img/test/graan/graan.png') }}" class="grain g13" alt="">
         <img src="{{ asset('img/test/graan/graanFlipped.png') }}" class="grain g14" alt="">
         <img src="{{ asset('img/test/graan/graan.png') }}" class="grain g15" alt="">
-
     </header>
 
     <main class="products">
-
         <h1>Producten</h1>
 
-
-        @guest
-
-            <a href="{{ route('login') }}">
-                Inloggen
-            </a>
-
-            <a href="{{ route('register') }}">
-                Registreren
-            </a>
-
-        @endguest
-
-
-        @auth
-
-            <p>
-                Ingelogd als {{ auth()->user()->name }}
-            </p>
-
-            <a href="{{ route('cart.index') }}">
-                Winkelmandje
-            </a>
-
-            <a href="{{ route('products.create') }}">
-                Product toevoegen
-            </a>
-
-            <form action="{{ route('logout') }}" method="POST">
-                @csrf
-
-                <button type="submit">
-                    Uitloggen
-                </button>
-            </form>
-
-        @endauth
-
+        <a href="{{ route('products.create') }}">
+            Product toevoegen
+        </a>
 
         <hr>
 
-
         @foreach ($products as $product)
-
             <article class="product">
-
-                <h2>
-                    {{ $product->name }}
-                </h2>
+                <h2>{{ $product->name }}</h2>
 
                 <p>
                     Prijs: € {{ $product->price }}
@@ -112,47 +76,30 @@
                     {{ $product->description }}
                 </p>
 
-
                 <a href="{{ route('products.show', $product) }}">
                     Bekijken
                 </a>
 
+                <a href="{{ route('products.edit', $product) }}">
+                    Bewerken
+                </a>
 
-                @auth
+                <form
+                    action="{{ route('products.destroy', $product) }}"
+                    method="POST"
+                >
+                    @csrf
+                    @method('DELETE')
 
-                    <a href="{{ route('products.edit', $product) }}">
-                        Bewerken
-                    </a>
-
-
-                    <form action="{{ route('cart.add', $product) }}" method="POST">
-                        @csrf
-
-                        <button type="submit">
-                            In winkelmandje
-                        </button>
-                    </form>
-
-
-                    <form action="{{ route('products.destroy', $product) }}" method="POST">
-                        @csrf
-                        @method('DELETE')
-
-                        <button type="submit">
-                            Verwijderen
-                        </button>
-                    </form>
-
-                @endauth
-
+                    <button type="submit">
+                        Verwijderen
+                    </button>
+                </form>
             </article>
 
             <hr>
-
         @endforeach
-
     </main>
 
 </body>
-
 </html>
