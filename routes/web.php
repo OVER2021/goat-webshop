b5a52f4980751e557444a9cb088b28ef1c13aaec
INDRATHEDESTROYER <D308873@edu.curio.nl>
2026-09-17 11:38:51 +0200

    Revert "Inloggen systeem en winkelmandje toegevoegd"
    This reverts commit 9d99a9ea7527b78a5209a2dadcf98e03ae26e42a.

--- routes/web.php
+++ routes/web.php
@@ -1,21 +1,12 @@
 <?php
 
-use App\Http\Controllers\CartController;
 use App\Http\Controllers\ProductController;
 use Illuminate\Support\Facades\Route;
 
-Route::redirect('/', '/products');
-
 Route::resource('products', ProductController::class);
 
-Route::middleware('auth')->group(function () {
-    Route::get('/cart', [CartController::class, 'index'])->name('cart.index');
+Route::redirect('/', '/products');// <--- voor automatisch brengen naar ...(home)pagina
 
-    Route::post('/cart/add/{product}', [CartController::class, 'add'])
-        ->name('cart.add');
-
-    Route::delete('/cart/remove/{product}', [CartController::class, 'remove'])
-        ->name('cart.remove');
-});
-
-require __DIR__.'/auth.php';
+// Route::get('/', function () {
+//     return view('welcome');
+// });
